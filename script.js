56d353c1516fe78e4649df98af5fa9228a904195
Davi Ribeiro <davi.ribeirodosanjos@gmail.com>
2021-01-06 11:09:50 -0300

    Fazendo modificações para merge

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const container = document.getElementById('container');
 
+const body = document.querySelector('body');
+const imgCogumelo = document.createElement('div');
+imgCogumelo.classList.add('cogumelo');
+body.appendChild(imgCogumelo);
+
 const coluna = (numeroColuna) => {  
     const colunas = document.createElement('div');
     colunas.classList.add(`coluna${numeroColuna}`, 'colunas');
+    colunas.addEventListener('click',jogada);
     for(let i = 0; i < 6; i++) {
         let circulo = document.createElement('div');
         circulo.classList.add('circulos');
@@ -13,6 +19,43 @@ const coluna = (numeroColuna) => {
 
 const addContainer = () => {
     for(let i = 0; i < 7; i++) {
-        coluna(i)
+        coluna(i);
     }
 }
+
+const jogador1 = () => {}
+
+const jogador2 = () => {}
+
+let contador = 0;
+let cogumelo = 0;
+const jogada = () => {
+
+    valorCogumelo()
+    alterarCogumelo()
+
+}
+
+const valorCogumelo = () => {
+    if(contador % 2 === 0) {
+        cogumelo = 1;
+        contador += 1;
+        return cogumelo
+    }
+    if(contador % 2 !== 0) {
+        cogumelo = 2;
+        contador += 1;
+        return cogumelo
+    }    
+}
+
+const alterarCogumelo = () => {
+    if(cogumelo === 1) {
+        imgCogumelo.style.backgroundImage = "url('img/super-mario-bros-3-toad-super-mario-bros-the-lost-levels-mario-bros-png-clip-art.png')";
+    }
+    if(cogumelo === 2) {
+        imgCogumelo.style.backgroundImage = "url('img/png-transparent-super-mario-advance-4-super-mario-bros-3-super-mario-world-skateboarding-text-super-mario-bros-rectangle.png')";
+    }
+}
+
+addContainer()
