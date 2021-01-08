4b877300514d1715482628014633e809fcf03a93
Hugo <costarafaelhugo@gmail.com>
2021-01-07 21:57:46 -0300

    alinhamentos corrigidos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.getElementById('container')
 const colunaSelecao = document.getElementsByClassName('colunas')
 const circuloSelecao = document.getElementsByClassName('circulos')
 
-const criaColuna = (numeroColuna) => {  
+const criaColuna = (numeroColuna) => {
     const colunas = document.createElement('div')
     colunas.classList.add(`coluna${numeroColuna}`, 'colunas')
     colunas.addEventListener('click', jogada)
@@ -26,7 +26,7 @@ let cogumelo = 0;
 const jogada = (e) => {
 
     valorCogumelo()
-    
+
     selecionaColuna(e)
 }
 
@@ -43,7 +43,7 @@ const valorCogumelo = () => {
 const selecionaColuna = (e) => {
     let alvo = e.target.classList[0];
 
-    if(alvo == circuloSelecao[0].classList[0]) {
+    if (alvo == circuloSelecao[0].classList[0]) {
         alvo = e.target.parentElement.classList[0]
     }
     const elemento = document.getElementsByClassName(`${alvo}`)
@@ -57,25 +57,26 @@ const criaCogumelo = (elemento) => {
 
     alterarCogumelo(imgCogumelo)
 
-    for(let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         let conteudoCirculo = elemento[0].children[i]
 
-        if(conteudoCirculo.children.length === 1) {
+        if (conteudoCirculo.children.length === 1) {
             conteudoCirculo = elemento[0].children[i - 1]
             conteudoCirculo.appendChild(imgCogumelo)
             condicaoVitoria(elemento)
             break
-        } else if(i === 5) {
+        } else if (i === 5) {
             conteudoCirculo.appendChild(imgCogumelo)
         }
     }
 }
 
 const alterarCogumelo = (imgCogumelo) => {
-    if(cogumelo === 1) {
+    if (cogumelo === 1) {
         imgCogumelo.style.backgroundImage = "url('img/green_cogu.png')";
+
     }
-    if(cogumelo === 2) {
+    if (cogumelo === 2) {
         imgCogumelo.style.backgroundImage = "url('img/red_cogu.png')";
     }
 }
@@ -83,29 +84,47 @@ const alterarCogumelo = (imgCogumelo) => {
 const condicaoVitoria = (elemento) => {
 
     condicaoVitoriaVertical(elemento)
+    condicaoVitoriaHorizontal(elemento)
 
 }
 
 const condicaoVitoriaVertical = (coluna) => {
-    
+
     let vCogumelo = [0, 0, 0, 0, 0, 0]
 
-    for(let i = 5; i >= 0; i--){
-        if(coluna[0].children[i].children[0] !== undefined)
-        vCogumelo[i] = coluna[0].children[i].children[0].style.backgroundImage
+    for (let i = 5; i >= 0; i--) {
+        if (coluna[0].children[i].children[0] !== undefined)
+            vCogumelo[i] = coluna[0].children[i].children[0].style.backgroundImage
     }
 
-    if(vCogumelo[0] === vCogumelo[1] && vCogumelo[1] === vCogumelo[2] && vCogumelo[2] === vCogumelo[3] && vCogumelo[3] !== 0) {
+    if (vCogumelo[0] === vCogumelo[1] && vCogumelo[1] === vCogumelo[2] && vCogumelo[2] === vCogumelo[3] && vCogumelo[3] !== 0) {
         console.log('vc venceu')
-    } else if(vCogumelo[1] === vCogumelo[2] && vCogumelo[2] === vCogumelo[3] && vCogumelo[3] === vCogumelo[4] && vCogumelo[4] !== 0) {
+    } else if (vCogumelo[1] === vCogumelo[2] && vCogumelo[2] === vCogumelo[3] && vCogumelo[3] === vCogumelo[4] && vCogumelo[4] !== 0) {
         console.log('vc venceu')
-    } else if(vCogumelo[2] === vCogumelo[3] && vCogumelo[3] === vCogumelo[4] && vCogumelo[4] === vCogumelo[5] && vCogumelo[5] !== 0) {
+    } else if (vCogumelo[2] === vCogumelo[3] && vCogumelo[3] === vCogumelo[4] && vCogumelo[4] === vCogumelo[5] && vCogumelo[5] !== 0) {
         console.log('vc venceu')
     }
 }
 
-const condicaoVitoriaDiagonal = () => {
+const condicaoVitoriaHorizontal = (coluna) => {
+    let vCogumelo = [
+        0, 0, 0, 0, 0, 0, 0,
 
+    ]
+    console.log(coluna)
+    for (let i = 6; i >= 0; i--) {
+        if (coluna[0].children[i].children[0] !== undefined)
+            vCogumelo[i] = coluna[0].children[i].children[0].style.backgroundImage
+    }
+
+    if (vCogumelo[0] === vCogumelo[1] && vCogumelo[1] === vCogumelo[2] && vCogumelo[2] === vCogumelo[3] && vCogumelo[3] !== 0) {
+        console.log('vc venceu')
+    } else if (vCogumelo[1] === vCogumelo[2] && vCogumelo[2] === vCogumelo[3] && vCogumelo[3] === vCogumelo[4] && vCogumelo[4] !== 0) {
+        console.log('vc venceu')
+    } else if (vCogumelo[2] === vCogumelo[3] && vCogumelo[3] === vCogumelo[4] && vCogumelo[4] === vCogumelo[5] && vCogumelo[5] !== 0) {
+        console.log('vc venceu')
+    }
 }
+
 
 addContainer();
